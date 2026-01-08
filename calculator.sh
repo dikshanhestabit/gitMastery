f0aedd28b09a7213074504eb2677811ad1eb3980
Diksha Nain <dikshanhestabit@gmail.com>
2026-01-08 15:58:56 +0530

    feat: add input validation

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,34 @@
-div() {
-  echo $(($1 * $2))   # ❌ BUG: should be division
+#!/bin/bash
+
+validate() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Error: two numbers required"
+    exit 1
+  fi
 }
 
+add() {
+  validate "$1" "$2"
+  echo $(($1 + $2))
+}
+
+sub() {
+  validate "$1" "$2"
+  echo $(($1 - $2))
+}
+
+mul() {
+  validate "$1" "$2"
+  echo $(($1 * $2))
+}
+
+div() {
+  validate "$1" "$2"
+  echo $(($1 * $2))   # ❌ BUG STILL HERE
+}
+
+add 5 3
+sub 5 3
+mul 5 3
 div 10 2
 
